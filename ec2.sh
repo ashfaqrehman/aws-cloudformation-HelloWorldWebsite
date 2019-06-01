79fce1c9b3c9611dbf6d45c76d8605fbffd691bf
(Ash)Ashfaq Rehman <ashfaq.arif@gmail.com>
2019-06-01 12:56:18 -0400

    update directory structure

--- ec2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name WebServer1 --template-body file://ec2.json
--- vpc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name SampleNetworkCrossStack --template-body file://vpc.yaml
