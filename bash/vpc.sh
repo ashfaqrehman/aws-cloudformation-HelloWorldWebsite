b003f51a4835f8c535234227f2c4f934b269dec0
(Ash)Ashfaq Rehman <ashfaq.arif@gmail.com>
2019-06-01 13:49:54 -0400

    Add VPC_2AZs

--- bash/vpc.sh
+++ bash/vpc.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name SampleNetworkCrossStack --template-body file://vpc.yaml
+#!/usr/bin/env bash
+
+region="us-east-1"
+profile="default"
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+aws cloudformation create-stack \
+    --stack-name AshfaqRehman-VPC \
+    --template-body file://${dir}/../cloudformation/vpc.yaml \
+    --parameters \
+        "ParameterKey=EnvironmentName,ParameterValue=VPC_2AZs"
