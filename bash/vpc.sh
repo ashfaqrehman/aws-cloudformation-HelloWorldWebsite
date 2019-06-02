7c9a3e72887bae6ee3446829aab21ecc88df4279
(Ash)Ashfaq Rehman <ashfaq.arif@gmail.com>
2019-06-02 03:08:32 -0400

    added testScaleUpPolicy.sh and testScaleDownPolicy.sh

--- bash/vpc.sh
+++ bash/vpc.sh
@@ -5,9 +5,11 @@ profile="default"
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#aws cloudformation create-stack \
-aws cloudformation update-stack \
+#aws cloudformation update-stack \
+aws cloudformation create-stack \
     --stack-name AshfaqRehman-VPC \
     --template-body file://${dir}/../cloudformation/vpc.yaml \
+    --region ${region} \
+    --profile ${profile} \
     --parameters \
         "ParameterKey=EnvironmentName,ParameterValue=VPC_2AZs"
